4cd7070b252479ce6f7e1f580134e6fe5c2c8da6
xeniaprokhorova <82230607+xeniaprokhorova@users.noreply.github.com>
2024-08-20 12:36:35 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -13,20 +14,35 @@ import org.w3c.dom.Element;
 
 public class Main {
 
+    private static void insertCatalogData(PreparedStatement preparedStatement, int id, String uuid, Date dateTime, String company) throws SQLException {
+        preparedStatement.setInt(1, id);
+        preparedStatement.setDate(2, dateTime);
+        preparedStatement.setString(3, company);
+        preparedStatement.setString(4, uuid);
+    }
+
+    private static void insertPlantData(PreparedStatement pstmtSelect, String value1, String value2, String value3, String value4, Double value5, int value6, String uuid) throws SQLException {
+        pstmtSelect.setString(1, value1);
+        pstmtSelect.setString(2, value2);
+        pstmtSelect.setString(3, value3);
+        pstmtSelect.setString(4, value4);
+        pstmtSelect.setDouble(5, value5);
+        pstmtSelect.setInt(6, value6);
+        pstmtSelect.setString(7, uuid);
+    }
+
     public static void main(String[] args) {
         String url = "jdbc:postgresql://localhost:5432/postgres";
         String username = "postgres";
         String password = "111111";
 
         try {
-
             File dir = new File("src");
             File[] files = dir.listFiles((d, name) -> name.endsWith(".xml"));
 
             int id = 0; //переменная для добавление id в таблицу D_CAT_CATALOG
 
             for (File xmlFile : files) {
-
                 //для работы с XML
                 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
                 DocumentBuilder builder = factory.newDocumentBuilder();
@@ -48,9 +64,8 @@ public class Main {
                 Element catalogElement = (Element) currentNode;
 
                 uuid = catalogElement.getAttribute("uuid");
-                String dateString = catalogElement.getAttribute("date");
                 //парсинг date для добавления в таблицу с типом данных TimeStamp
-                java.util.Date date = new SimpleDateFormat("dd.MM.yyyy").parse(dateString);
+                java.util.Date date = new SimpleDateFormat("dd.MM.yyyy").parse(catalogElement.getAttribute("date"));
 
                 Date dateTime = new java.sql.Date(date.getTime());
 
@@ -60,20 +75,14 @@ public class Main {
                 PreparedStatement pstmtCATALOG = conn.prepareStatement("INSERT INTO d_cat_catalog (id, delivery_date, company, uuid) VALUES (?, ?, ?, ?)");
                 PreparedStatement pstmtSelect = conn.prepareStatement("SELECT 1 FROM d_cat_catalog WHERE id = ? AND delivery_date = ? AND company = ? AND uuid = ?");
 
-                pstmtSelect.setInt(1, id);
-                pstmtSelect.setDate(2, dateTime);
-                pstmtSelect.setString(3, company);
-                pstmtSelect.setString(4, uuid);
+                insertCatalogData(pstmtSelect, id, uuid, dateTime, company);
 
                 ResultSet rs1 = pstmtSelect.executeQuery();
 
                 // проверка на наличие данных в таблице, чтобы не добавлять одно и то же несколько раз
                 if (!rs1.next()) {
                     //добавление данных
-                    pstmtCATALOG.setInt(1, id);
-                    pstmtCATALOG.setDate(2, dateTime);
-                    pstmtCATALOG.setString(3, company);
-                    pstmtCATALOG.setString(4, uuid);
+                    insertCatalogData(pstmtCATALOG, id, uuid, dateTime, company);
                     pstmtCATALOG.executeUpdate();
                 }
 
@@ -90,7 +99,6 @@ public class Main {
 
                     //по всем элементам внутри PLANT
                     for (int j = 0; j < node.getChildNodes().getLength(); j++) {
-
                         String value1 = element.getElementsByTagName("COMMON").item(0).getTextContent();
                         String value2 = element.getElementsByTagName("BOTANICAL").item(0).getTextContent();
                         String value3 = element.getElementsByTagName("ZONE").item(0).getTextContent();
@@ -98,27 +106,13 @@ public class Main {
                         String priceStr = element.getElementsByTagName("PRICE").item(0).getTextContent();
                         Double value5 = Double.valueOf(priceStr.substring(1));
                         int value6 = Integer.parseInt(element.getElementsByTagName("AVAILABILITY").item(0).getTextContent());
-                        String value7 = uuid;
 
-                        // Set the values in the select prepared statement
-                        pstmtSelect1.setString(1, value1);
-                        pstmtSelect1.setString(2, value2);
-                        pstmtSelect1.setString(3, value3);
-                        pstmtSelect1.setString(4, value4);
-                        pstmtSelect1.setDouble(5, value5);
-                        pstmtSelect1.setInt(6, value6);
-                        pstmtSelect1.setString(7, uuid);
+                        insertPlantData(pstmtSelect1, value1, value2, value3, value4, value5, value6, uuid);
 
                         ResultSet rs = pstmtSelect1.executeQuery();
 
                         if (!rs.next()) {
-                            pstmtPLANTS.setString(1, value1);
-                            pstmtPLANTS.setString(2, value2);
-                            pstmtPLANTS.setString(3, value3);
-                            pstmtPLANTS.setString(4, value4);
-                            pstmtPLANTS.setDouble(5, value5);
-                            pstmtPLANTS.setInt(6, value6);
-                            pstmtPLANTS.setString(7, value7);
+                            insertPlantData(pstmtPLANTS, value1, value2, value3, value4, value5, value6, uuid);
                             pstmtPLANTS.executeUpdate();
                         }
                     }
